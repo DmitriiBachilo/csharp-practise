a14b237589cfffb889b615ee2f8378a3d1fe9c78
DmitriiBachilo <dimamn7@gmail.com>
2020-03-17 21:57:48 +0300

    Add Lect5.LetterA

---
+++ Lect5.LetterA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lect5.LetterA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a message");
+            string str = Console.ReadLine();
+            int valueLength = str.Length;
+            Console.WriteLine($"Count of numbers =" + valueLength);
+        }
+    }
+}
