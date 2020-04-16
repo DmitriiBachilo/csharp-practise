2d899b47b8e2819c33a96d058ce167bc239f3c5c
DmitriiBachilo <dimamn7@gmail.com>
2020-04-16 20:34:18 +0300

    Add Lect_12

---
+++ Lecture12/Lecture12/UniqueItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture12
+{
+    class UniqueItem
+    {
+        private int _id;
+
+        private static int LastId = 1;
+
+        public UniqueItem()
+        {
+            _id = LastId + 1;
+            LastId = _id;
+        }
+
+    }
+}
