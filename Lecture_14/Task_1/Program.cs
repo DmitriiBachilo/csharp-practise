c3e6604fd457bd40c5dd3a7c49036dc825fcf3ba
DmitriiBachilo <dimamn7@gmail.com>
2020-04-23 20:06:27 +0300

    Add Task1

---
+++ Lecture_14/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lect14.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello, guys!";
+            string hello2 = hello.Pruning();
+            Console.WriteLine(hello2);
+        }
+    }
+}
---
+++ Lecture_14/Task_1/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Lect14.Task1
+{
+    static class StringExtensions
+    {
+        public static string Pruning(this string str)
+        {
+            const int length = 5;
+            if (str.Length > length)
+            {
+                return str.Substring(0, length) + "...";
+            }
+            return str;
+        }
+    }
+}
