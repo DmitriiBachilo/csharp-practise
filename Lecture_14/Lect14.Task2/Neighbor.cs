e09eaf1fec04a27ed156424cf87e3761da46b6f2
DmitriiBachilo <dimamn7@gmail.com>
2020-04-23 20:25:20 +0300

    Add Task2

---
+++ Lecture_14/Lect14.Task2/Neighbor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lect14.Task2
+{
+    class Neighbor
+    {
+        public string FullName { get; set; }
+
+        public int FlatNumber { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+    }
+}
---
+++ Lecture_14/Lect14.Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace Lect14.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Neighbor> floorNeighbors = new List<Neighbor>()
+            {
+                new Neighbor(){FullName = "Ivanov", FlatNumber = 2, PhoneNumber = "375296544554"},
+                new Neighbor(){FullName = "Petrov", FlatNumber = 3, PhoneNumber = "375258766748"},
+                new Neighbor(){FullName = "Sidorov", FlatNumber = 4, PhoneNumber = "375333145498"}
+            };
+
+            Console.Write("Enter flat number, please: ");
+            int flatnumber;
+
+            while (!int.TryParse(Console.ReadLine(), out flatnumber))
+            {
+                Console.Write("Please, enter flat number again: ");
+            }
+
+            foreach (var neighbor in floorNeighbors)
+            {
+                if (neighbor.FlatNumber == flatnumber)
+                {
+                    Console.WriteLine($"{neighbor.FullName} - {neighbor.PhoneNumber}");
+                    break;
+                }
+            }
+        }
+    }
+}
